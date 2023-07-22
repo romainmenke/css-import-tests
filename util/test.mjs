7665c3c09fc721b5b0b0d08d4d4282c1be32f797
Romain Menke <romainmenke@gmail.com>
2023-07-22 13:55:25 +0200

    wip

--- util/test.mjs
+++ util/test.mjs
@@ -133,6 +133,10 @@ export async function createTest(browser, testPath) {
 								minify: false
 							});
 
+							console.log('----------------');
+							console.log(`---- ${ testPath } ------`);
+							console.log(code.toString());
+
 							res.end(code);
 						} catch (e) {
 							requestHandlerError = e;
