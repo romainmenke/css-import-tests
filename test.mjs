7115e4df984a77e9c8163b8d9d1d507a0ab692fd
Romain Menke <romainmenke@gmail.com>
2023-08-09 17:01:26 +0200

    add more structure to these tests

--- test.mjs
+++ test.mjs
@@ -6,6 +6,8 @@ import './prepare-postcss-import-dev.mjs';
 
 const { createTestSafe } = await import('./util/test.mjs')
 
+const onlyRunTest = process.argv.slice(2)[0];
+
 const firefox = await puppeteer.launch({
 	headless: 'new',
 	product: 'firefox',
@@ -19,7 +21,9 @@ const testCases = (await fs.readdir('./tests', { withFileTypes: true, recursive:
 	return dirent.isFile() && dirent.name === 'style.css'
 }).map(dirent => {
 	return path.relative('tests', dirent.path);
-})
+}).filter((x) => {
+	return !onlyRunTest || x.includes(onlyRunTest);
+});
 
 testCases.sort();
 
@@ -29,7 +33,7 @@ for (const testCase of testCases) {
 	// https://bugzilla.mozilla.org/show_bug.cgi?id=1844683
 	// Ideally we test everything against one browser as we aren't trying to create a second WPT.
 	// Firefox supports `support()` conditions, Chrome better supports cyclic imports.
-	if (testCase.includes('cycles/006') || testCase.includes('cycles/007') || testCase.includes('cycles/008')) {
+	if (testCase.includes('001-core-features/cycles/006') || testCase.includes('002-at-media/cycles/001') || testCase.includes('003-at-layer/cycles/001')) {
 		results.push(
 			await createTestSafe(chrome, ['tests', ...testCase.split(path.sep)])
 		);
